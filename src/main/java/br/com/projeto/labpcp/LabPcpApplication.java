fa638ee39c776bc985d81088bc8ea3b01774404f
Vinicius Goulart Alves <vini.goulartalves@gmail.com>
2024-04-21 12:50:45 -0300

    Commit inicial

---
+++ src/main/java/br/com/projeto/labpcp/LabPcpApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.labpcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabPcpApplication.class, args);
+    }
+
+}
